6afc47e05f5dfd9cc9651488a0dc4897513ef51a
spmega <spmega4567@gmail.com>
2017-11-22 20:06:47 -0500

    Added code to maintain our trees

--- src/shashank/second_robotplayer/Gardener.java
+++ src/shashank/second_robotplayer/Gardener.java
@@ -3,6 +3,9 @@ package shashank.second_robotplayer;
 import battlecode.common.Clock;
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
+import battlecode.common.TreeInfo;
+
+import java.util.Arrays;
 
 import static shashank.second_robotplayer.RobotPlayer.rc;
 
@@ -19,6 +22,8 @@ public class Gardener {
     private static Direction SOUTHEAST_DIR = new Direction((float) Math.toRadians(-60));
     private static Direction SOUTH_DIR = new Direction((float) Math.toRadians(-120));
 
+    private static boolean foundEmptySpot = false;
+
     public static void loop(){
         while (true){
             try{
@@ -27,17 +32,30 @@ public class Gardener {
                 //RobotPlayer.tryMove(RobotPlayer.randomDirection());
 
                 //search for empty spot to take up residence
-
-                //if we have found a spot start planting
-                plantTrees();
-                //maintain the trees around you
-
+                if(!foundEmptySpot)
+                    findSpot();
+                else {
+                    //if we have found a spot start planting
+                    plantTrees();
+                    //maintain the trees around you
+                    maintainTrees();
+                }
 
                 Clock.yield();
             } catch (GameActionException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private static void findSpot() {
+        /* Ways to find empty spots:
+         *  - Just build some lumberjacks to free up some space
+         *  - Wander around until we find some empty spots
+         *     (Not feasible since it will depend on luck
+        */
+
+        
     }
 
     private static void plantTrees() throws GameActionException {
@@ -55,5 +73,30 @@ public class Gardener {
                 rc.plantTree( SOUTH_DIR );
             }
         }
+    }
+
+    private static void maintainTrees() throws GameActionException {
+        TreeInfo[] allyTreeInfo = rc.senseNearbyTrees(-1, rc.getTeam());
+
+        //organize the trees by least to greatest health first
+        if (allyTreeInfo.length >= 2){
+            Arrays.sort(allyTreeInfo, (TreeInfo tree1, TreeInfo tree2) -> {
+                if(tree1.health < tree2.health)
+                    return -1;
+                else if (tree1.health > tree2.health)
+                    return 1;
+
+                return 0;
+            });
+
+            if (rc.canWater() && rc.canWater(allyTreeInfo[0].ID))
+                rc.water(allyTreeInfo[0].ID);
+
+        } else if (allyTreeInfo.length == 1){
+            if (rc.canWater() && rc.canWater(allyTreeInfo[0].ID))
+                rc.water(allyTreeInfo[0].ID);
+        }
+
+
     }
 }
